e34196f62961e881936e61522922e6235e2b93b5
Dominik Lorenc <dominiklorenc1270@gmail.com>
2026-07-24 15:36:14 +0200

    feat(proxy): enhance authentication flow in proxy middleware
    - Added checks to redirect users based on authentication status when accessing the dashboard and login routes.
    - Implemented console logging for the token to aid in debugging.
    - Updated matcher configuration to include the login path for better route handling.

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,17 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
 
-
-
-
 export async function proxy(request: NextRequest) {
-    const token = request.cookies.get("token");
-    if (!token) {
-        return NextResponse.redirect(new URL("/login", request.url))
-    }
-    return NextResponse.next();
-};
+  const token = request.cookies.get("token");
+  if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
+  if (token && request.nextUrl.pathname === "/login") {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-    matcher: ["/dashboard/:path*"]
-}
+  matcher: ["/dashboard/:path*", "/login"],
+};
